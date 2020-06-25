344f80fce81ceda32f723af6117969f49af8d003
alexcos20 <alex.coseru@gmail.com>
2020-06-25 06:02:11 -0700

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/oceandb-elasticsearch-driver',
-    version='0.3.2',
+    version='0.3.3',
     zip_safe=False,
 )
